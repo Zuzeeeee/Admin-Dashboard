59ca3c40a2a9fa3c4d86b5946a6cf0e01d68ec91
Jose-Alves-Neto <jose.alves.neto@ccc.ufcg.edu.br>
2024-06-18 12:51:02 -0300

    Login and middleware

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,14 +1,11 @@
 import styles from '../ui/login/login.module.css';
+import { authenticate } from '../lib/actions';
+import LoginForm from '../ui/login/loginForm/loginForm';
 
 const LoginPage = () => {
   return (
     <div className={styles.container}>
-      <form action='' className={styles.form}>
-        <h1>Login</h1>
-        <input type='text' placeholder='username'></input>
-        <input type='password' placeholder='password'></input>
-        <button>Login</button>
-      </form>
+      <LoginForm />
     </div>
   );
 };
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -13,6 +13,7 @@ import {
   MdHelpCenter,
   MdLogout,
 } from 'react-icons/md';
+import { auth, signOut } from '@/app/auth';
 
 const menuItems = [
   {
@@ -77,19 +78,21 @@ const menuItems = [
   },
 ];
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  const session = await auth();
+
   return (
     <div className={styles.container}>
       <div className={styles.user}>
         <Image
           className={styles.userImage}
-          src='/next.svg'
+          src={session.user.image ?? '/noavatar.png'}
           alt=''
           width='50'
           height='50'
         ></Image>
         <div className={styles.userDetail}>
-          <span className={styles.username}>John Joe</span>
+          <span className={styles.username}>{session.user.name}</span>
           <span className={styles.userTitle}>Administrator</span>
         </div>
       </div>
@@ -103,9 +106,17 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
-      <button className={styles.logout}>
-        <MdLogout /> Logout
-      </button>
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+      >
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+      </form>
     </div>
   );
 };
